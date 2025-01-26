0a579314ff25df5bc62e2491f7a8782fa3a40061
Reuben Thomas <rrt@sc3d.org>
2025-01-26 12:08:39 +0000

    Update to match change to Chambercourt

--- mrmatt/mrmatt_game.py
+++ mrmatt/mrmatt_game.py
@@ -123,96 +123,105 @@ into them, after which they disappear.
                 if block == Tile.FOOD:
                     self.food += 1
 
-    def can_move(self, velocity: Vector2) -> bool:
-        newpos = self.hero.position + velocity
-        block = self.get(newpos)
-        if block in self.move_onto_tiles:
-            return True
-        if block in self.pushable_tiles:
-            new_pushedpos = self.hero.position + velocity * 2
-            return velocity.y == 0 and self.get(new_pushedpos) == Tile.EMPTY
-        return False
+    def fall(self, pos: Vector2, new_hero_pos: Vector2, dx: float) -> None:
+        while self.get(pos) in self.pushable_tiles and pos.y >= 0:
+            self.plummet(pos, new_hero_pos, dx)
+            pos -= Vector2(0, 1)
+            dx = 0
 
-    def do_play(self) -> None:
-        def fall(pos: Vector2, new_hero_pos: Vector2, dx: float) -> None:
-            while self.get(pos) in self.pushable_tiles and pos.y >= 0:
-                plummet(pos, new_hero_pos, dx)
-                pos -= Vector2(0, 1)
-                dx = 0
+    # FIXME: make a test level for these rules.
+    # Document the physics somewhere.
+    #
+    # 1. Bombs are like rocks, except that when dropped on a rock or another
+    # bomb, they will explode, destroying what they land on.
+    #
+    # 2. Boxes are like rocks, except that you can drop three other objects
+    # (rocks, bombs and boxes) into them; after the third, they disappear.
+    #
+    # 3. Bombs must fall to explode, while boxes swallow things pushed over
+    # them.
 
-        # FIXME: make a test level for these rules.
-        # Document the physics somewhere.
-        #
-        # 1. Bombs are like rocks, except that when dropped on a rock or another
-        # bomb, they will explode, destroying what they land on.
-        #
-        # 2. Boxes are like rocks, except that you can drop three other objects
-        # (rocks, bombs and boxes) into them; after the third, they disappear.
-        #
-        # 3. Bombs must fall to explode, while boxes swallow things pushed over
-        # them.
-
-        def plummet(pos: Vector2, new_hero_pos: Vector2, push_dx: float) -> None:
-            new_pos = None
-            dx = push_dx
-            block = self.get(pos)
-            self.set(pos, Tile.EMPTY)
-            while pos.y < self.level_height:
-                block_below = self.get(pos + Vector2(0, 1))
-                if block_below == Tile.EMPTY:
-                    new_pos = pos + Vector2(0, 1)
-                    if new_pos + Vector2(0, 1) == new_hero_pos:
-                        self.dead = True
-                elif block_below in self.box_tiles:
-                    pos += Vector2(0, 1)
-                    if block_below == Tile.BOX3:
-                        block = Tile.BOX2
-                        self.box_sound.play()
-                    elif block_below == Tile.BOX2:
-                        block = Tile.BOX1
-                        self.box_sound.play()
-                    elif block_below == Tile.BOX1:
-                        block = Tile.EMPTY
-                        self.debox_sound.play()
-                elif block == Tile.BOMB and block_below in self.explosion_trigger_tiles:
-                    self.bomb_sound.play()
-                    if block_below != Tile.BRICK:
-                        pos += Vector2(0, 1)
+    def plummet(self, pos: Vector2, new_hero_pos: Vector2, push_dx: float) -> None:
+        new_pos = None
+        dx = push_dx
+        block = self.get(pos)
+        self.set(pos, Tile.EMPTY)
+        while pos.y < self.level_height:
+            block_below = self.get(pos + Vector2(0, 1))
+            if block_below == Tile.EMPTY:
+                new_pos = pos + Vector2(0, 1)
+                if new_pos + Vector2(0, 1) == new_hero_pos:
+                    self.dead = True
+            elif block_below in self.box_tiles:
+                pos += Vector2(0, 1)
+                if block_below == Tile.BOX3:
+                    block = Tile.BOX2
+                    self.box_sound.play()
+                elif block_below == Tile.BOX2:
+                    block = Tile.BOX1
+                    self.box_sound.play()
+                elif block_below == Tile.BOX1:
                     block = Tile.EMPTY
-                    break
-                elif block_below == Tile.STONE:
-                    if dx == 0:
-                        dx = 1 if self.hero.position.x < pos.x else -1
-                    if (
-                        self.get(pos + Vector2(dx, 0)) == Tile.EMPTY
-                        and self.get(pos + Vector2(dx, 1)) in self.fall_into_tiles
-                    ):
-                        new_pos = pos + Vector2(dx, 1)
-                    elif (
-                        self.get(pos + Vector2(-dx, 0)) == Tile.EMPTY
-                        and self.get(pos + Vector2(-dx, 1)) in self.fall_into_tiles
-                    ):
-                        new_pos = pos + Vector2(-dx, 1)
-                        dx = -dx
-                    else:
-                        break
-                    if new_pos == new_hero_pos:
-                        new_pos -= Vector2(0, 1)
-                        self.dead = True
-                    if self.get(new_pos) in self.box_tiles:
-                        new_pos -= Vector2(0, 1)
+                    self.debox_sound.play()
+            elif block == Tile.BOMB and block_below in self.explosion_trigger_tiles:
+                self.bomb_sound.play()
+                if block_below != Tile.BRICK:
+                    pos += Vector2(0, 1)
+                block = Tile.EMPTY
+                break
+            elif block_below == Tile.STONE:
+                if dx == 0:
+                    dx = 1 if self.hero.position.x < pos.x else -1
+                if (
+                    self.get(pos + Vector2(dx, 0)) == Tile.EMPTY
+                    and self.get(pos + Vector2(dx, 1)) in self.fall_into_tiles
+                ):
+                    new_pos = pos + Vector2(dx, 1)
+                elif (
+                    self.get(pos + Vector2(-dx, 0)) == Tile.EMPTY
+                    and self.get(pos + Vector2(-dx, 1)) in self.fall_into_tiles
+                ):
+                    new_pos = pos + Vector2(-dx, 1)
+                    dx = -dx
                 else:
                     break
-                assert new_pos is not None
-                pos = new_pos
-                self.set(pos, block)
-                self.draw()
-                self.show_status()
-                self.show_screen()
-                self.set(pos, Tile.EMPTY)
-            self.stone_sound.play()
+                if new_pos == new_hero_pos:
+                    new_pos -= Vector2(0, 1)
+                    self.dead = True
+                if self.get(new_pos) in self.box_tiles:
+                    new_pos -= Vector2(0, 1)
+            else:
+                break
+            assert new_pos is not None
+            pos = new_pos
             self.set(pos, block)
+            self.draw()
+            self.show_status()
+            self.show_screen()
+            self.set(pos, Tile.EMPTY)
+        self.stone_sound.play()
+        self.set(pos, block)
 
+    def try_move(self, delta: Vector2) -> bool:
+        newpos = self.hero.position + delta
+        block = self.get(newpos)
+        if block == Tile.FOOD:
+            self.collect_sound.play()
+            self.food -= 1
+            return True
+        elif block in self.pushable_tiles:
+            new_pushedpos = newpos + delta
+            if delta.y == 0 and self.get(new_pushedpos) == Tile.EMPTY:
+                new_block_pos = newpos + delta
+                self.set(newpos, Tile.EMPTY)
+                self.set(new_block_pos, block)
+                if self.get(new_block_pos + Vector2(0, 1)) in self.fall_into_tiles:
+                    self.fall(new_block_pos, newpos, delta.x)
+        elif block in self.move_onto_tiles:
+            return True
+        return False
+
+    def update_map(self) -> None:
         # Handle rock falls from rocks left unsupported by last move.
         # Put Matt into the map data for collision detection.
         self.set(self.hero.position, Tile.HERO)
@@ -225,29 +234,17 @@ into them, after which they disappear.
                 if block == Tile.EMPTY:
                     pos_above = pos + Vector2(0, -1)
                     block_above = self.get(pos_above)
-                    if block_above == Tile.STONE:
-                        fall(pos_above, self.hero.position, 0)
+                    if block_above in self.pushable_tiles:
+                        self.fall(pos_above, self.hero.position, 0)
         self.set(self.hero.position, Tile.EMPTY)
         if self.dead:
             self.die()
-
-        newpos = self.hero.position + self.hero.velocity
-        block = self.get(newpos)
-        if block == Tile.FOOD:
-            self.collect_sound.play()
-            self.food -= 1
-        elif block in self.pushable_tiles:
-            new_rockpos = self.hero.position + (self.hero.velocity * 2)
-            self.set(new_rockpos, block)
-            if self.get(new_rockpos + Vector2(0, 1)) in self.fall_into_tiles:
-                fall(new_rockpos, newpos, self.hero.velocity.x)
-        self.set(newpos, Tile.EMPTY)
 
     def die(self) -> None:
         self.die_sound.play()
         self.game_surface.blit(
             self.die_image,
-            self.game_to_screen((int(self.hero.position.x), int(self.hero.position.y))),
+            self.game_to_screen(self.hero.position),
         )
         self.show_status()
         self.show_screen()
